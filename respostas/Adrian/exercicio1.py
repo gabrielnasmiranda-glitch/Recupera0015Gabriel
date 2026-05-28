f0deb699a097b7d14d354ce0a388740077448d91
Adrian <contatoadriantech@gmail.com>
2026-05-28 20:04:21 -0300

    criacao exercicio 1

--- respostas/Adrian/exercicio1.py
+++ respostas/Adrian/exercicio1.py
@@ -10,4 +10,4 @@ def calcular_media(nota1, nota2 ):
 media_calculada =calcular_media(nota1=n1, nota2=n2)
 print (f"o aluno {aluno} obteve a seguinte media: {media_calculada}")
 
-        
+        # o codigo acima e o exercicio 2

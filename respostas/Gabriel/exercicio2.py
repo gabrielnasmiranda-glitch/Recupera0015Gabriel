6dddce8cf122a519564097f13fbb6fe47463ea08
Gabriel <gabrielnasmiranda@gmail.com>
2026-05-28 19:59:10 -0300

    exercicio2 completo

--- respostas/Gabriel/exercicio2.py
+++ respostas/Gabriel/exercicio2.py
@@ -5,6 +5,16 @@ n2 = float (input("Digite a segunda nota do aluno:"))
 def calcular_media(nota1,nota2):
     return (nota1 + nota2) / 2
 
-print (f"O aluno {aluno} obteve a seguinte média: {calcular_media(n1,n2)}")
+media = calcular_media(n1,n2)
+
+print (f"O aluno {aluno} obteve a seguinte média: {media}")
 
 
+if media >= 60:
+    print (f"{aluno} Aprovado!")
+
+if media >=40:
+    print(f"{aluno} está de Recuperação!")
+
+else: 
+    print(f"{aluno} Reprovado!")
